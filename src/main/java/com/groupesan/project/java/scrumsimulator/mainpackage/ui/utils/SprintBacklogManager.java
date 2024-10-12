aafd4cc57286b8240a4a7037ea077cc774c86657
avijay48 <avijay48@asu.edu>
2024-10-11 20:20:30 -0700

    69 The sprint backlog is not getting updated in the Edit Sprint Window

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/utils/SprintBacklogManager.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/utils/SprintBacklogManager.java
@@ -1,5 +1,4 @@
 package com.groupesan.project.java.scrumsimulator.mainpackage.ui.utils;
-
 import com.groupesan.project.java.scrumsimulator.mainpackage.impl.UserStory;
 
 import java.util.ArrayList;
@@ -8,32 +7,17 @@ import java.util.List;
 public class SprintBacklogManager {
     public List<UserStory> generateSprintBacklog(List<UserStory> userStoryList, int totalStoryPoints){
         List<UserStory> sprintBacklog = new ArrayList<>();
-        selectUserStories(userStoryList, sprintBacklog, new ArrayList<>(), 0, totalStoryPoints, 0);
-        return sprintBacklog;
-    }
+        userStoryList.sort((a,b)->Double.compare(b.getBusinessValue(),a.getBusinessValue()));
+        for(UserStory userStory: userStoryList){
+            int storyPoints = (int)userStory.getPointValue();
 
-    /**
-     * Select User stories with highest business value which are less than user story point in the form.
-     * */
-    private void selectUserStories(List<UserStory> stories, List<UserStory> sprintBacklog, List<UserStory> currentSelection,
-                           int start, int remainingPoints, int currentValue) {
-
-        if (currentSelection.stream().mapToInt(us -> (int) us.getBusinessValue()).sum() >
-                sprintBacklog.stream().mapToInt(us -> (int) us.getBusinessValue()).sum()) {
-            sprintBacklog.clear();
-            sprintBacklog.addAll(currentSelection); // Update sprint backlog with current selection of user story
-        }
-
-        for (int i = start; i < stories.size(); i++) {
-            UserStory story = stories.get(i);
-            int storyPoints = (int) story.getPointValue();
-
-            if (storyPoints <= remainingPoints) {
-                currentSelection.add(story); // Add story to current selection
-                selectUserStories(stories, sprintBacklog, currentSelection, i + 1, remainingPoints - storyPoints,
-                        currentValue + (int) story.getBusinessValue());
-                currentSelection.remove(currentSelection.size() - 1);
+            if(storyPoints<=totalStoryPoints){
+                sprintBacklog.add(userStory);
+                totalStoryPoints -= storyPoints;
             }
+
         }
+        return sprintBacklog;
+
     }
 }
